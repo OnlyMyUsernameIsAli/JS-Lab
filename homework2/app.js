e6a240564d472533a0c4b61b4a0e66276774fe1a
Ali_Nouri <amynn8008@gmail.com>
2023-07-21 21:04:43 +0330

    exercises were done

---
+++ homework2/app.js
@@ -0,0 +1,7 @@
+let num1 = Number(prompt("enter number :"))
+let num2 = Number(prompt("enter number :"))
+let num3 = Number(prompt("enter number :"))
+CenterNum(num1, num2, num3)
+
+// let InNum = Number(prompt("enter number :"))
+// Collector(InNum)
---
+++ homework2/functions.js
@@ -0,0 +1,34 @@
+function CenterNum(num1, num2, num3) {
+    if (num1 > num3) {
+        let x = num3
+        num3 = num1
+        num1 = x
+    }
+    if (num1 > num2) {
+        let x = num2
+        num2 = num1
+        num1 = x
+    }
+    if (num2 > num3) {
+        let x = num2
+        num2 = num3
+        num3 = x
+    }
+    document.write("The Middle number : ", num2)
+    console.log(num1, num2, num3)
+}
+
+// let OutNum = 0
+// function Collector(InNum) {
+//     if (InNum !== 0) {
+//         OutNum += InNum;
+//         functionCaller(InNum);
+//     }
+//     if(InNum === 0){
+//         document.write("Total numbers : ", OutNum)
+//     }
+// }
+// function functionCaller(InNum) {
+//     InNum = Number(prompt("enter number :"))
+//     Collector(InNum)
+// }
