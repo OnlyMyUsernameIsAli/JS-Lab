226b88dfa2c3c9f0de774cf661dc126e92c9f94a
Ali_Nouri <amynn8008@gmail.com>
2023-08-08 15:18:07 +0330

    send to git-hub

---
+++ homework8/1/func.js
@@ -0,0 +1,20 @@
+function func1() {
+    let img = document.querySelector(".wall1");
+    let src = img.getAttribute("src");
+    document.querySelector(".img__preview").setAttribute("src", src);
+}
+function func2() {
+    let img = document.querySelector(".wall2");
+    let src = img.getAttribute("src");
+    document.querySelector(".img__preview").setAttribute("src", src);
+}
+function func3() {
+    let img = document.querySelector(".wall3");
+    let src = img.getAttribute("src");
+    document.querySelector(".img__preview").setAttribute("src", src);
+}
+function func4() {
+    let img = document.querySelector(".wall4");
+    let src = img.getAttribute("src");
+    document.querySelector(".img__preview").setAttribute("src", src);
+}
